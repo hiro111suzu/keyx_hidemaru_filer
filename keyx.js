eb0d56b68e8a785a484cacd64067c7ffbacfe227
hiro111suzu <hiro111suzu@gmail.com>
2022-09-22 06:42:04 +0900

    v1pre

--- keyx.js
+++ keyx.js
@@ -16,15 +16,16 @@ var ignore = { //- shift/ctrl/alt
 	0xa4: true,
 	0xa5: true
 };
-for ( var num = 1; num < 230; ++ num ) {
-	if ( _iskeydown( num ) && ! ignore[ num ] ) break;
+for ( var keycode = 1; keycode < 230; ++ keycode ) {
+	if ( _iskeydown( keycode ) && ! ignore[ keycode ] ) break;
 }
-key += _code2key( num );
 
 //. init
 var actx = {};
 _common_lib();
 var ini_data = _get_inidata( ScriptFullName.replace( '.js', '.ini' ) );
+key += ini_data.code2key[ keycode ] || keycode;
+
 var user_conf = {
 	debug_soft: ini_data.config.debug_soft
 		? ini_data.config.debug_soft.env_expand()
@@ -193,106 +194,6 @@ function _time_log( title ) {
 //.. _iskeydown
 function _iskeydown( num ) {
 	return getKeyState( num ) & 0x8000;
-}
-
-//.. _code2key
-function _code2key( code ) {
-	return {
-		0x01: 'mouse_L',
-		0x02: 'mouse_R',
-		0x04: 'mouse_M',
-		0x09: 'Tab',
-		0x0c: 'Clear',
-		0x0d: 'Enter',
-		0x13: 'Pause',
-		0x1b: 'Esc',
-		0x20: 'Space',
-		0x21: 'PageUp',
-		0x22: 'PageDown',
-		0x23: 'End',
-		0x24: 'Home',
-		0x25: 'Left',
-		0x26: 'Up',
-		0x27: 'Right',
-		0x28: 'Down',
-		0x2d: 'Insert',
-		0x2e: 'Delete',
-		0x30: '0',
-		0x31: '1',
-		0x32: '2',
-		0x33: '3',
-		0x34: '4',
-		0x35: '5',
-		0x36: '6',
-		0x37: '7',
-		0x38: '8',
-		0x39: '9',
-		0x41: 'A',
-		0x42: 'B',
-		0x43: 'C',
-		0x44: 'D',
-		0x45: 'E',
-		0x46: 'F',
-		0x47: 'G',
-		0x48: 'H',
-		0x49: 'I',
-		0x4A: 'J',
-		0x4B: 'K',
-		0x4C: 'L',
-		0x4D: 'M',
-		0x4E: 'N',
-		0x4F: 'O',
-		0x50: 'P',
-		0x51: 'Q',
-		0x52: 'R',
-		0x53: 'S',
-		0x54: 'T',
-		0x55: 'U',
-		0x56: 'V',
-		0x57: 'W',
-		0x58: 'X',
-		0x59: 'Y',
-		0x5A: 'Z',
-		0xba: 'Colon', //:*
-		0xbb: 'Semicolon', //;+
-		0xbc: 'Comma', //,
-		0xbd: 'Hyphen', //-=
-		0xbe: 'Period', //.>
-		0xbf: 'Slash', // /?
-		0xc0: 'Atmark', //@`
-		0xdb: 'LBrcket', // [{
-		0xdc: 'Yen', //\|
-		0xdd: 'RBracket', //]}
-		0xde: 'Caret', //^~
-		0xe2: '_',
-	
-	//- テンキー
-		0x60: 't_0',
-		0x61: 't_1',
-		0x62: 't_2',
-		0x63: 't_3',
-		0x64: 't_4',
-		0x65: 't_5',
-		0x66: 't_6',
-		0x67: 't_7',
-		0x68: 't_8',
-		0x69: 't_9',
-		0x6a: 't_asterisk',
-		0x6b: 't_plus',
-	
-		0x70: 'F1',
-		0x71: 'F2',
-		0x72: 'F3',
-		0x73: 'F4',
-		0x74: 'F5',
-		0x75: 'F6',
-		0x76: 'F7',
-		0x77: 'F8',
-		0x78: 'F9',
-		0x79: 'F10',
-		0x7A: 'F11',
-		0x7B: 'F12'
-	}[ code ] || code;
 }
 
 //. func 汎用
